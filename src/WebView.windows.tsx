366001da48053ebcdb6a0b979d8712939ee4f303
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2024-06-28 18:22:28 +0200

    Misc fixes

--- src/WebView.windows.tsx
+++ src/WebView.windows.tsx
@@ -10,12 +10,7 @@
  * Licensed under the MIT License.
  */
 
-import {
-  forwardRef,
-  useCallback,
-  useImperativeHandle,
-  useRef,
-} from 'react';
+import { forwardRef, useCallback, useImperativeHandle, useRef } from 'react';
 
 import {
   type ImageSourcePropType,
